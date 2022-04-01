4ed9c19dfa9a50887ed76794ce8cb3ef270f5db7
JAMS <JAMS@oums-jams12.materials.ox.ac.uk>
2022-04-01 22:13:08 +0100

    Need to style the cards better to ensure uniform height

--- app.py
+++ app.py
@@ -22,6 +22,40 @@ def laser_controller():
     call_lq = laser_quantum.call_lq()
     return render_template('laser_controller.html')
 
+@app.route('/toptica_controller')
+def toptica_controller():
+    # call_lq = laser_quantum.call_lq()
+    return render_template('toptica_controller.html')
+
+@app.route('/montana_vnc')
+def montana_vnc():
+    # call_lq = laser_quantum.call_lq()
+    return render_template('montana_vnc.html')
+
+@app.route('/aom_enable')
+def aom_enable():
+    # call_lq = laser_quantum.call_lq()
+    return render_template('aom_enable.html')
+
+@app.route('/p_scan_labview')
+def pl_scan_labview():
+    # call_lq = laser_quantum.call_lq()
+    return render_template('pl_scan_labview.html')
+
+@app.route('/ple_labview')
+def ple_labview():
+    # call_lq = laser_quantum.call_lq()
+    return render_template('ple_labview.html')
+
+@app.route('/wavemeter')
+def wavemeter():
+    # call_lq = laser_quantum.call_lq()
+    return render_template('wavemeter.html')
+
+@app.route('/camera')
+def camera():
+    # call_lq = laser_quantum.call_lq()
+    return render_template('camera.html')
 
 # @app.route("/")
 # def hello_world():
