75618722b12e8c2bb2fae48568301486a01728e6
JAMS <JAMS@oums-jams12.materials.ox.ac.uk>
2022-04-01 18:53:24 +0100

    Added capability of opening laser quantum control software

---
+++ laser_quantum.py
@@ -0,0 +1,4 @@
+import subprocess
+
+# This is not a good way of doing this, but works right now. better to search for the file in the directory.
+oprog = subprocess.call(["C:\Program Files (x86)\Laser Quantum Ltd\LQ Remote App\RemoteApp Laser Control.exe"])
--- spd3303x.py
+++ spd3303x.py
@@ -9,8 +9,8 @@ class PowerSupply:
     def __init__(self):
         f = open('config\\pyvisarc.json') # need to get correct library file 
         lib = json.load(f)
-        lib['config']['visa_lib']
-        self.rm = pyvisa.ResourceManager(lib)
+        visa_lib = lib['config']['visa_lib']
+        self.rm = pyvisa.ResourceManager(visa_lib)
         return
 
     def connect(self, addr=None, USB=False, ASRL=False, GPIB=False):
@@ -22,7 +22,7 @@ class PowerSupply:
             for r in resources:
                 if USB:
                     if 'USB0' in r:
-                        self.addr(r)
+                        self.addr = r
         try:
             self.inst = self.rm.open_resource(self.addr)
         except:
@@ -31,4 +31,18 @@ class PowerSupply:
     
     def channel_output(self, ch='CH1', cmnd='OFF'):
         self.inst.write("OUTPut " + ch + "," + cmnd)
+        return
+    
+    def set_voltage(self, ch='CH1', volts=0):
+        self.inst.write('{:s}: VOLTage {:.3f}'.format(ch, volts))
+
+    def read_voltage(self, ch='CH1'):
+        self.inst.query('MEASure: VOLTage? ' + ch )
+        return
+
+    def set_current(self, ch='CH1', current=0):
+        self.inst.write('{:s}: CURRent {:.3f}'.format(ch, current))
+
+    def read_current(self, ch='CH1'):
+        self.inst.query('MEASure: CURRent? ' + ch )
         return
