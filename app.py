63896a455e85856eedc9ea45c9c520ea871a31f7
JAMS <JAMS@oums-jams12.materials.ox.ac.uk>
2022-04-01 19:24:31 +0100

    Some code for graphically opening up laser controller

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #!usr/bin/env python
 import spd3303x
+import laser_quantum
 from flask import Flask
 from flask import render_template
 from flask import request
@@ -15,6 +16,12 @@ def power_supply():
     ps = spd3303x.PowerSupply()
     ps.connect(USB=True) # for the time being, just connect when the webpage is opened
     return render_template('power_supply.html')
+
+@app.route('/laser_controller')
+def laser_controller():
+    call_lq = laser_quantum.call_lq()
+    return render_template('laser_controller.html')
+
 
 # @app.route("/")
 # def hello_world():
--- laser_quantum.py
+++ laser_quantum.py
@@ -1,4 +1,6 @@
 import subprocess
 
 # This is not a good way of doing this, but works right now. better to search for the file in the directory.
-oprog = subprocess.call(["C:\Program Files (x86)\Laser Quantum Ltd\LQ Remote App\RemoteApp Laser Control.exe"])
+def call_lq():
+    oprog = subprocess.call(["C:\Program Files (x86)\Laser Quantum Ltd\LQ Remote App\RemoteApp Laser Control.exe"])
+    return
